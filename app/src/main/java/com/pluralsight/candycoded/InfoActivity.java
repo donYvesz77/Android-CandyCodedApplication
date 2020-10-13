576bfb1f4a4023f5b3fabd0b7531c7832cb5859f
Zavel Y Ogola <yveszavel@gmail.com>
2020-10-13 07:17:36 +0200

    Completed task 3

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -43,4 +43,10 @@ public class InfoActivity extends AppCompatActivity {
     // ***
     // TODO - Task 3 - Launch the Phone Activity
     // ***
+    public void createPhoneIntent(View view){
+        Intent dial = new Intent(Intent.ACTION_DIAL);
+        Uri phoneNum = Uri.parse("tel:0123456789");
+        dial.setData(phoneNum);
+        startActivity(dial);
+    }
 }
